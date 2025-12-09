53c27609180f35c8a905573e1f1284f6074320b6
lachfoy <lachie.foy7@gmail.com>
2025-12-09 16:29:43 +1100

    chore: un-inline getViewMatrix

--- src/engine/Camera.cpp
+++ src/engine/Camera.cpp
@@ -2,6 +2,21 @@
 
 #include "Engine.h"
 
+glm::mat4 Camera::getViewMatrix() const
+{
+	glm::mat4 view = glm::mat4(1.0f);
+
+	// apply rotation (pitch->yaw->roll)
+	view = glm::rotate(view, glm::radians(rotation.x), glm::vec3(1, 0, 0));
+	view = glm::rotate(view, glm::radians(rotation.y), glm::vec3(0, 1, 0));
+	view = glm::rotate(view, glm::radians(rotation.z), glm::vec3(0, 0, 1));
+
+	// apply translation
+	view = glm::translate(view, -position);
+
+	return view;
+}
+
 void Camera::update(float dt)
 {
 #ifdef WITH_EDITOR
